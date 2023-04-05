c648c20023a098826e970a14c1d4f7cfe3f7d1bc
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-04-05 16:49:48 -0600

    fix linters error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
-    has_many :comments, foreign_key: 'post_id', dependent: :destroy
-    has_many :likes, foreign_key: 'post_id', dependent: :destroy
-    belongs_to :author, class_name: 'User'
-  
-    def recent_comments
-      comments.order(created_at: :desc).limit(5)
-    end
-  
-    after_save :update_post_counter
-  
-    private
-  
-    def update_post_counter
-      author.update(posts_counter: author.posts.count)
-    end
+  has_many :comments, foreign_key: 'post_id', dependent: :destroy
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy
+  belongs_to :author, class_name: 'User'
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.count)
+  end
 end
