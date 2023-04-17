26361fc9daae08218988cdd0d1cef2f3f338c0f1
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-04-17 14:24:05 -0600

    Update user controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,36 +1,45 @@
 require 'rails_helper'
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
+  before(:each) do
+    @user1 = User.create(
+      name: 'User 1',
+      photo: 'https://unsplash.com/photos/Hekapoo',
+      bio: 'User 1 bio',
+      posts_counter: 0
+      )
+    end
+    
+    describe 'GET /index' do
     it 'returns http success' do
-      get '/users/'
+      get users_path
       expect(response).to have_http_status(:success)
     end
 
     it 'returns render index template' do
-      get '/users/'
+      get users_path
       expect(response).to render_template(:index)
     end
 
     it 'body includes correct placeholder text' do
-      get '/users/'
-      expect(response.body).to include('Users#index')
+      get users_path
+      expect(response.body).to include(@user1.name)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/:id'
+      get users_path(@user1)
       expect(response).to have_http_status(:success)
     end
 
     it 'returns render show template' do
-      get '/users/:id'
+      get user_path(@user1)
       expect(response).to render_template(:show)
     end
 
-    it 'body includes correct placeholder text' do
-      get '/users/:id'
-      expect(response.body).to include('Users#show')
+    it 'body includes correct photo text' do
+      get users_path
+      expect(response.body).to include(@user1.name)
     end
   end
 end
