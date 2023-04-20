bfbba7a481ff59f9b7d655de3420c9593ee93db6
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-04-20 08:05:03 -0600

    Modify ability tu use the cancancan

---
+++ app/models/ability.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Post, author_id: user.id
+      can :update, Post, author_id: user.id
+      can :destroy, Post, author_id: user.id
+      can :create, Comment, author_id: user.id
+      can :update, Comment, author_id: user.id
+      can :destroy, Comment, author_id: user.id
+    end
+  end
+end
