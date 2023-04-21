59bf5f8299b418d674ba0ab9f8d7d1d38198e240
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-04-21 08:39:38 -0600

    Fix linters error

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,23 +1,22 @@
 class Api::CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-    def index
-      @post = Post.find(request.params['post_id'])
-      render json: @post.comments.all
-    end
-  
-    def create
-      comment_params = params.require(:comment).permit(:author_id, :post_id, :text)
-  
-      @user = User.find(comment_params[:author_id])
-      @post = Post.find(comment_params[:post_id])
-      @comment = @post.comments.create(comment_params)
-      @comment.post_id = @post.id
-      if @comment.save
-        render json: { status: 'great¡ congratulation¡' }
-      else
-        render json: { status: 'Fail!, dem!' }
-      end
+  def index
+    @post = Post.find(request.params['post_id'])
+    render json: @post.comments.all
+  end
+
+  def create
+    comment_params = params.require(:comment).permit(:author_id, :post_id, :text)
+
+    @user = User.find(comment_params[:author_id])
+    @post = Post.find(comment_params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+      render json: { status: 'great¡ congratulation¡' }
+    else
+      render json: { status: 'Fail!, dem!' }
     end
   end
-  
+end
