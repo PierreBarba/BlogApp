1681e313abcbc21a6632b48f8efe32927ad9db41
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-04-20 18:26:14 -0600

    Routes Modify & Add new routes to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'users#index'
+
   resources :users do
    resources :posts do 
     resources :comments, :likes
+   end
+  end
+  #route api
+  namespace :api do
+    resources :users do
+      resources :posts do 
+       resources :comments
+    end
    end
   end
 end
